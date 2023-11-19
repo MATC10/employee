4d59739ac7e934ba21b0bcf2a7ad0ba906201e65
MATC10 <miguelangel.s309600@cesurformacion.com>
2023-11-19 15:39:10 +0100

    First project commit

---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.employee.service;
+
+import com.example.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
